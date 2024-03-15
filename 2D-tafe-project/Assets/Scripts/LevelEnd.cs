193fd7468106854cd8499e24dda4f1ed404579eb
Wesselo <101975862+WesseloDev@users.noreply.github.com>
2024-03-15 11:59:35 +1100

    adds object that transitions to next scene

---
+++ 2D-tafe-project/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    [Tooltip("Name of the scene file to go to after reaching this, excluding file extension")]
+    [SerializeField] private string targetScene;
+
+    private BoxCollider2D trigger;
+
+    void Start()
+    {
+        trigger = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Player") return;
+
+        if (targetScene == "" || targetScene == null) Debug.LogWarning("No target scene set!"); return;
+
+        SceneHandler.sh.ChangeScene(targetScene);
+    }
+}
